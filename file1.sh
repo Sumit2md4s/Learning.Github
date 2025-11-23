e08dfcea49ee6583dae67ffa5e8e112ff5c57861
Sumit Zarkhande <sumitzarkhande@gmail.com>
2025-11-23 13:41:54 +0530

    This is code for addition of two numbers

---
+++ file1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+### Author - Sumit 
+### Date:23-10-2025
+### This file im creating to learn github and commands related to github such as git init, git add, git commit, git log, git diff, git status, git push 
+
+
+
+
+# Project 1 
+#Addition of two numbers
+
+x=a+b
+
